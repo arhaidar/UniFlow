6804c5649db6d924cd45c94ff62c4791a3d498ee
unknown <mkwon8385@gmail.com>
2025-04-05 16:56:23 -0700

    gitignore file and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,27 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/UniFlow/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: { 
+    outDir: 'root',
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [react()],
 })
+
+// this one does not deploy properly via github default pages
+// export default defineConfig({
+//   base: '/UniFlow/',
+//   plugins: [react()],
+// })
